139bb75d166c75ce101ed379efafeeb90668cd35
Nata Arena <nataarena20@gmail.com>
2026-01-31 16:04:40 -0300

    Remove contato e sobre

--- recipes/views.py
+++ recipes/views.py
@@ -1,13 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Create your views here.
 
 def home(request):
-    return HttpResponse('HOME 1')
+    return render(request, 'recipes/home.html', context={
+        'name' : 'Nata',
+    })
 
-def sobre(request):
-    return HttpResponse('sobre')
-
-def contato(request):
-    return HttpResponse('contato')
